3c4f0bc9c525de801efe19933e697ae4960b7a8a
Veeru Natarajan <veeruns@yahoo.com>
2018-10-02 13:29:30 -0700

    Updated alexa-skillalexa-skill.go .

--- alexa-skill.go
+++ alexa-skill.go
@@ -237,7 +237,7 @@ func HandleRequest(ctx context.Context, i AlexaRequest) (AlexaResponse, error) {
 				asays = fmt.Sprintf("The answer you have given is %d", qanswer)
 				resp.Ssay(asays)
 				resp.Response.ShouldEndSession = "false"
-				resp.AddDialogDirective("Dialog.Delegate", "", "", &updatedintent)
+				resp.AddDialogDirective("Dialog.ConfirmIntent", "", "", nil)
 			}
 
 		default:
