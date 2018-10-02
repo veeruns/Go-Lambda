f9408d55f957fe2b483a8db5c58950d4d1328194
Veeru Natarajan <veeruns@yahoo.com>
2018-10-02 09:52:15 -0700

    Updated alexa-skill.go.

--- alexa-skill.go
+++ alexa-skill.go
@@ -66,7 +66,7 @@ type DialogDirective struct {
 	UpdatedIntent *Intent `json:"updatedIntent,omitempty"`
 }
 
-//Creates a response with flag to create either an SSML or plaintext outputSpeech
+//CreatesResponse with flag to create either an SSML or plaintext outputSpeech
 func CreateResponse(flag bool) *AlexaResponse {
 	var resp AlexaResponse
 	resp.Version = "1.0"
