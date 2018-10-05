2155500f0819d05835dbefdfc1133efa6cbd8c2c
Veeru Natarajan <veeruns@yahoo.com>
2018-10-05 12:08:56 -0700

    Updated alexa-skill.go.

--- alexa-skill.go
+++ alexa-skill.go
@@ -291,12 +291,12 @@ func HandleRequest(ctx context.Context, i AlexaRequest) (AlexaResponse, error) {
 			}
 		case "IN_PROGRESS":
 			datanum := i.Session.Attributes.String
-
+			spew.Dump(datanum)
 			for _, v := range datanum {
 				switch val := v.(type) {
 				case string:
 					var err error
-					questionnumber, err = strconv.Atoi(string(val{"questionnumber"}))
+					questionnumber, err = strconv.Atoi(val)
 					fmt.Printf("Did you get questionnumber %d %s\n", questionnumber, err.Error())
 				default:
 					fmt.Printf("There is default case")
