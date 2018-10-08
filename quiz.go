22199b7d23061e1e7cb9ca0f873748f8363431ac
Veeru Natarajan <veeruns@yahoo.com>
2018-10-08 12:09:45 -0700

    Updated quiz.go.

--- quiz.go
+++ quiz.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-"bytes"
-"context"
-"encoding/json"
-"fmt"
-"strconv"
-"time"
-"math/rand"
-"reflect"
-"github.com/aws/aws-lambda-go/lambda"
-"github.com/davecgh/go-spew/spew"
-)
-
 func mathquiz(){
   var quizanswer int
   resp = CreateResponse(false)
