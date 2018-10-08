c474eaa48874a00da2630a81e7f17f56cd1b0e90
Veeru Natarajan <veeruns@yahoo.com>
2018-10-08 10:58:16 -0700

    Updated dyndb.go.

--- dyndb.go
+++ dyndb.go
@@ -5,3 +5,5 @@ import (
    "github.com/aws/aws-sdk-go/service/dynamodb"
    "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
+//US east 1 is where the data for capitals is
+var db = dynamodb.New(session.New(),aws.NewConfig().WithRegion("us-east-1"))
