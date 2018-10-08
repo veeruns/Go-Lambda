d4aa0f70acda9dd106477e6d0c54c161bbc65c65
Veeru Natarajan <veeruns@yahoo.com>
2018-10-08 11:53:47 -0700

    Updated quiz.go.

---
+++ quiz.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+"bytes"
+"context"
+"encoding/json"
+"fmt"
+"strconv"
+"time"
+"math/rand"
+"reflect"
+"github.com/aws/aws-lambda-go/lambda"
+"github.com/davecgh/go-spew/spew"
+)
