22a4699ad55174ddac51e86742954a68a9fbbd32
Veeru Natarajan <veeruns@yahoo.com>
2018-10-08 10:16:09 -0700

    Updated upload-to-dynamodb.goalexa-skill alexa-skill.zip .

--- Utils/upload-to-dynamodb.go
+++ Utils/upload-to-dynamodb.go
@@ -16,6 +16,30 @@ import (
 type CapitalInfo struct {
     Country string `json:"Country"`
     Capital string `json:"Capital"`
+}
 
 
+func Readfile() []CapitalInfo {
+  raw,err := ioutil.ReadFile("../Data/country-to-capital.json")
+  if(err != nil) {
+    fmt.Println(err.Error())
+    os.Exit(1)
+  }
+  var capitalinfos []CapitalInfo
+  json.Unmarshal(raw, &capitalinfos)
+  return capitalinfos
+
+}
+
+func main() {
+  sess, err := session.NewSession(&aws.Config{
+        Region: aws.String("us-east-1")},
+    )
+    if err != nil {
+        fmt.Println("Error creating session:")
+        fmt.Println(err.Error())
+        os.Exit(1)
+    }
+    svc := dynamodb.New(sess)
+    
 }
