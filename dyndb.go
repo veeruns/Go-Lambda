0dc1c505fb824cc147484840e8bd3523f3ac687e
Veeru Natarajan <veeruns@yahoo.com>
2018-10-08 11:01:10 -0700

    Updated dyndb.go.

--- dyndb.go
+++ dyndb.go
@@ -7,3 +7,43 @@ import (
 )
 //US east 1 is where the data for capitals is
 var db = dynamodb.New(session.New(),aws.NewConfig().WithRegion("us-east-1"))
+
+type CapitalInfo struct {
+    Country string `json:"Country"`
+    City string `json:"City"`
+}
+
+func getItem(country string) (*CapitalInfo, error) {
+    // Prepare the input for the query.
+    input := &dynamodb.GetItemInput{
+        TableName: aws.String("Capitals"),
+        Key: map[string]*dynamodb.AttributeValue{
+            "Country": {
+                S: aws.String(country),
+            },
+        },
+    }
+
+    // Retrieve the item from DynamoDB. If no matching item is found
+    // return nil.
+    result, err := db.GetItem(input)
+    if err != nil {
+        return nil, err
+    }
+    if result.Item == nil {
+        return nil, nil
+    }
+
+    // The result.Item object returned has the underlying type
+    // map[string]*AttributeValue. We can use the UnmarshalMap helper
+    // to parse this straight into the fields of a struct. Note:
+    // UnmarshalListOfMaps also exists if you are working with multiple
+    // items.
+    cty := new(CapitalInfo)
+    err = dynamodbattribute.UnmarshalMap(result.Item, cty)
+    if err != nil {
+        return nil, err
+    }
+
+    return cty, nil
+}
