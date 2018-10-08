dd1ab8a2575e58141202c4304f91aef1c02535e8
Veeru Natarajan <veeruns@yahoo.com>
2018-10-08 09:51:57 -0700

    Updated alexa-skillalexa-skill.zip compile-publish.sh .

---
+++ compile-publish.sh
@@ -0,0 +1,1 @@
+GOOS=linux GOARCH=amd64 go build -o alexa-skill alexa-skill.go && zip alexa-skill.zip alexa-skill && aws s3 cp alexa-skill.zip s3://veeruns && aws lambda  update-function-code --function-name golang_lamda --s3-bucket veeruns --s3-key alexa-skill.zip --publish
