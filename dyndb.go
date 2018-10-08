7bb1b438e38e675e40af812e5773eda902b4e880
Veeru Natarajan <veeruns@yahoo.com>
2018-10-08 12:22:46 -0700

    Updated dyndb.go.

--- dyndb.go
+++ dyndb.go
@@ -7,7 +7,7 @@ import (
 )
 //US east 1 is where the data for capitals is
 var db = dynamodb.New(session.New(),aws.NewConfig().WithRegion("us-east-1"))
-//CapitalInfo Strucuture ... not generic
+//CapitalInfo Strucuture  for loading capital json
 type CapitalInfo struct {
     Country string `json:"Country"`
     City string `json:"City"`
