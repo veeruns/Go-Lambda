899a2866ae5b1a19d300bcdda863bf932c67f224
Veeru Natarajan <veeruns@yahoo.com>
2018-10-12 11:11:10 -0700

    Updated setuid.go.

--- https-server/setuid.go
+++ https-server/setuid.go
@@ -9,27 +9,26 @@ import (
 func main() {
 	if syscall.Getuid() == 0 {
 		fmt.Printf("You are running as root, Dropping your privileges")
-    toprint,ferr := filetostring("/etc/letsencrypt/live/veeruns.raghavanonline.com/README")
-    if(ferr == nil){
-      fmt.Printf("README Doc is %s\n",toprint)
-    }else{}
-      fmt.Printf(ferr.Error())
-    }
-		err := syscall.Setuid(65534)
-		if err != nil {
-			fmt.Printf(err.Error())
-      _,nerr := filetostring("/etc/letsencrypt/live/veeruns.raghavanonline.com/README")
-      if(nerr == nil){
-        fmt.Printf("Something wrong it should not happen")
-      } else{
-        fmt.Printf("This is correct %s\n",nerr.Error())
-      }
+		toprint, ferr := filetostring("/etc/letsencrypt/live/veeruns.raghavanonline.com/README")
+		if ferr == nil {
+			fmt.Printf("README Doc is %s\n", toprint)
 		} else {
-
-
 		}
+		fmt.Printf(ferr.Error())
+	}
+	err := syscall.Setuid(65534)
+	if err != nil {
+		fmt.Printf(err.Error())
+		_, nerr := filetostring("/etc/letsencrypt/live/veeruns.raghavanonline.com/README")
+		if nerr == nil {
+			fmt.Printf("Something wrong it should not happen")
+		} else {
+			fmt.Printf("This is correct %s\n", nerr.Error())
+		}
+	} else {
 
 	}
+
 }
 
 func filetostring(filename string) (string, error) {
