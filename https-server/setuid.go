610b8aee78ca98cc8c12d4b70bff3fe01b5dbd8f
Veeru Natarajan <veeruns@yahoo.com>
2018-10-12 10:07:25 -0700

    Updated setuid.go.

---
+++ https-server/setuid.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"ioutil"
+	"syscall"
+)
+
+func main() {
+	if syscall.Getuid() == 0 {
+		fmt.Printf("You are running as root, Dropping your privileges")
+		err := syscall.Setuid(65534)
+		if err != nil {
+			fmt.Printf(err.Error())
+		} else {
+
+		}
+
+	}
+}
+
+func filetostring(filename string) (string, error) {
+	var output []byte
+	var err error
+	output, err = ioutil.ReadFile(filename)
+	if err != nil {
+		return _, err
+	} else {
+		return string(output), _
+	}
+}
