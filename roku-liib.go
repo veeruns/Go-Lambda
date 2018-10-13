b7208969f58142eed386bb9830aa45ab59c3d3fe
Veeru Natarajan <veeruns@yahoo.com>
2018-10-13 13:30:49 -0700

    Updated roku-liib.go.

---
+++ roku-liib.go
@@ -0,0 +1,39 @@
+package rokulib
+import (
+  "net/http"
+  "fmt"
+  "bytes"
+)
+
+func PowerOff(hostname string) bool {
+  var buff bytes.Buffer
+  var url bytes.Buffer
+  url.WriteString("http://")
+  url.WriteString(hostname)
+  url.WriteString("/keypress/poweron")
+  resp,err := http.Post(url.String(),"",&buff)
+  if(err != nil){
+    fmt.Printf("That did not work as intended %s\n",err.Error())
+    return false
+  } else{
+    fmt.Printf("The return string is %s\n",resp.Body)
+    return true
+  }
+}
+
+
+func PowerOn(hostname string) bool {
+  var buff bytes.Buffer
+  var url bytes.Buffer
+  url.WriteString("http://")
+  url.WriteString(hostname)
+  url.WriteString("/keypress/poweron")
+  resp,err := http.Post(url.String(),"",&buff)
+  if(err != nil){
+    fmt.Printf("That did not work as intended %s\n",err.Error())
+    return false
+  } else{
+    fmt.Printf("The return string is %s\n",resp.Body)
+    return true
+  }
+}
