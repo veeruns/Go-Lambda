16e0962b1f49d236a2dee6dc907beeebbe9a0d14
Veeru Natarajan <veeruns@yahoo.com>
2018-10-13 21:53:26 -0700

    Updated httpsServer.go.

--- httpsServer/httpsServer.go
+++ httpsServer/httpsServer.go
@@ -4,7 +4,6 @@ import (
 	// "fmt"
 	// "io"
 
-	"bytes"
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
@@ -90,36 +89,4 @@ func logMiddleware(next http.Handler) http.Handler {
 
 		next.ServeHTTP(w, req)
 	})
-}
-
-func PowerOff(hostname string) bool {
-	var buff bytes.Buffer
-	var url bytes.Buffer
-	url.WriteString("http://")
-	url.WriteString(hostname)
-	url.WriteString("/keypress/poweron")
-	resp, err := http.Post(url.String(), "", &buff)
-	if err != nil {
-		fmt.Printf("That did not work as intended %s\n", err.Error())
-		return false
-	} else {
-		fmt.Printf("The return string is %s\n", resp.Body)
-		return true
-	}
-}
-
-func PowerOn(hostname string) bool {
-	var buff bytes.Buffer
-	var url bytes.Buffer
-	url.WriteString("http://")
-	url.WriteString(hostname)
-	url.WriteString("/keypress/poweron")
-	resp, err := http.Post(url.String(), "", &buff)
-	if err != nil {
-		fmt.Printf("That did not work as intended %s\n", err.Error())
-		return false
-	} else {
-		fmt.Printf("The return string is %s\n", resp.Body)
-		return true
-	}
 }
