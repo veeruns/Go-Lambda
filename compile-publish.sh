890b0ce9a1f6475364637fca75b5a42317fcf25f
Veeru Natarajan <veeruns@yahoo.com>
2018-10-13 23:01:49 -0700

    Updated compile-publish.sh.

--- compile-publish.sh
+++ compile-publish.sh
@@ -1,2 +1,2 @@
-GOOS=linux GOARCH=amd64 go build -o alexa-skill alexa-skill.go dyndb.go capital-quiz.go && zip alexa-skill.zip alexa-skill && aws s3 cp alexa-skill.zip s3://veeruns && aws lambda  update-function-code --function-name golang_lamda --s3-bucket veeruns --s3-key alexa-skill.zip --publish
+GOOS=linux GOARCH=amd64 go build -o alexa-skill alexa-skill.go dyndb.go capital-quiz.go && zip -r alexa-skill.zip alexa-skill ssl/ && aws s3 cp alexa-skill.zip s3://veeruns && aws lambda  update-function-code --function-name golang_lamda --s3-bucket veeruns --s3-key alexa-skill.zip --publish
 rm -rf alexa-skill alexa-skill.zip
