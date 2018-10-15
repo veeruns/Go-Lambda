49e5d8d114156d999aa4fb1671d92072abb93e17
Veeru Natarajan <veeruns@yahoo.com>
2018-10-15 11:57:24 -0700

    Updated alexa-skill.go.

--- alexa-skill.go
+++ alexa-skill.go
@@ -316,7 +316,7 @@ func HandleRequest(ctx context.Context, i AlexaRequest) (AlexaResponse, error) {
 
 	case "switchofftv":
 		resp = CreateResponse(true)
-		say := CallEndPoint()
+		say := CallEndPoint("PowerOff")
 		fmt.Printf("Output from Endpoint is %s\n", say)
 		resp.Say(say)
 
