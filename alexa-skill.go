f6ba68af35d15e626037e527ec7b1c09690b8f4e
Veeru Natarajan <veeruns@yahoo.com>
2018-10-15 12:39:56 -0700

    Updated httpsServer.

--- alexa-skill.go
+++ alexa-skill.go
@@ -317,6 +317,7 @@ func HandleRequest(ctx context.Context, i AlexaRequest) (AlexaResponse, error) {
 	case "rokucontrol":
 		resp = CreateResponse(true)
 		functocall := i.Request.Intent.Slots["function"].Value
+		fmt.Printf("The request intent value is %s\n", functocall)
 		say := CallEndPoint(functocall)
 		fmt.Printf("Output from Endpoint is %s\n", say)
 		resp.Say(say)
