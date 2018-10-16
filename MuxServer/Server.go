e989d47906885944b9e9ff962879c90d53409e78
Veeru Natarajan <veeruns@yahoo.com>
2018-10-16 16:18:00 -0700

    Updated httpsServer.

---
+++ MuxServer/Server.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/gorilla/mux"
+
+	"github.com/veeruns/Go-Lambda/rokulib"
+)
+
+func RokuServer(w http.ResponseWriter, req *http.Request) {
+	var functocall string
+	var channeltocall string
+	functocall = req.URL.Query().Get("func")
+	channeltocall = req.URL.Query().Get("channel")
+	fmt.Printf("Raw url string %s\n", req.URL.RequestURI())
+	wholeurl, _ := url.Parse(req.URL.RequestURI())
+	//queryparams := wholeurl.Query()
+	fmt.Printf("The whole url is %s\n", wholeurl.String())
+	fmt.Printf("The query function is %s\n", functocall)
+	fmt.Printf("Channel change is %s\n", channeltocall)
+	m, _ := url.ParseQuery(wholeurl.RawQuery)
+	fmt.Println("Parameters are ", m)
+	fmt.Printf("Whole query is %s\n", m["channel"])
+	if len(req.TLS.PeerCertificates) > 0 {
+		fmt.Fprintf(w, "client common name: %+v\n", req.TLS.PeerCertificates[0].Subject.CommonName)
+		fmt.Fprintf(w, "Client OU %+v\n", req.TLS.PeerCertificates[0].Subject.OrganizationalUnit)
+		fmt.Printf("client common name: %+v\n", req.TLS.PeerCertificates[0].Subject.CommonName)
+		fmt.Printf("Client OU %+v\n", req.TLS.PeerCertificates[0].Subject.OrganizationalUnit)
+		//	fmt.Fprintf(w, " %s\n", req.TLS.PeerCertificates[0].Verify)
+	}
+	for _, certname := range req.TLS.PeerCertificates {
+		for _, dnsname := range certname.DNSNames {
+			fmt.Printf("SAN names listed %s\n", dnsname)
+		}
+	}
+	if strings.Compare(req.TLS.PeerCertificates[0].Subject.CommonName, "client-auth.raghavanonline.com") != 0 {
+
+		w.WriteHeader(http.StatusForbidden)
+		fmt.Fprintf(w, "meh")
+	} else {
+		w.Header().Set("Content-Type", "text/plain")
+		var works bool
+		switch {
+		case strings.Compare(functocall, "off") == 0:
+			works = rokulib.PowerOff("192.168.7.45:8060")
+			fmt.Printf(" Rokulib PowerOff returned %v\n", works)
+		case strings.TrimRight(functocall, "\n") == "on":
+			works = rokulib.PowerOn("192.168.7.45:8060")
+			fmt.Printf(" Rokulib PowerOn returned %v\n", works)
+		case len(channeltocall) > 0:
+			channelname := strings.TrimRight(channeltocall, "\n")
+			valuid := rokulib.ChannelHash[channelname]
+			fmt.Printf("Channel Name is %s and Channel id is %d\n", channelname, valuid)
+			works = rokulib.LaunchChannel("192.168.7.45:8060", valuid)
+			fmt.Printf("Rokulib returned the %v\n", works)
+		default:
+			works = false
+			fmt.Printf("We are calling default\n")
+		}
+
+		//"http://192.168.7.45:8060/keypress/powerOff",
+		var retbytes bytes.Buffer
+		retbytes.WriteString(functocall)
+		if works {
+			fmt.Printf("It Returned a bool")
+			retbytes.WriteString(" Achieved")
+		}
+
+		io.WriteString(w, retbytes.String())
+
+		//rokulib.GetResponses()
+	}
+	// fmt.Fprintf(w, "This is an example server.\n")
+	// io.WriteString(w, "This is an example server.\n")
+}
+
+func main() {
+	r := mux.NewRouter()
+	rokulib.InitLib()
+
+	caCert, err := ioutil.ReadFile("/etc/httpsServer/ssl/certs/CAcerts.pem")
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cfg := &tls.Config{
+		MinVersion:         tls.VersionTLS12,
+		ClientAuth:         tls.RequireAndVerifyClientCert,
+		InsecureSkipVerify: true,
+		ClientCAs:          caCertPool,
+	}
+	srv := &http.Server{
+		Addr: ":8080",
+		//		Handler:   mux,
+		TLSConfig: cfg,
+		Handler:   r,
+	}
+
+	http.ListenAndServeTLS(addr, certFile, keyFile, handler)
+	r.HandleFunc("/roku", Rokuserver)
+
+}
