1741e855580c01378032c07aae6bf145644f3450
Veeru Natarajan <veeruns@yahoo.com>
2018-10-16 11:51:12 -0700

    Updated alexa-skill.go.

--- alexa-skill.go
+++ alexa-skill.go
@@ -168,9 +168,7 @@ func (resp *AlexaResponse) Say(text string) {
 func ResolutionValue(input interface{}) string {
 	switch input.(type) {
 	case Slot:
-		var slotvalue Resolutions
-		slotvalue = input.(Resolutions)
-		fmt.Printf("Slot value is %s\n", slotvalue.ResolutionsPerAuthority[0].Values[0].Value.Name)
+		fmt.Printf("Slot value is %s\n", input.(Resolutions).ResolutionsPerAuthority[0].Values[0].Value.Name)
 	default:
 		fmt.Println("Do not care ", reflect.TypeOf(input))
 	}
