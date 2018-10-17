cc8232edd6ca7c7b41aa5430d48c6725492d6b95
Veeru Natarajan <veeruns@yahoo.com>
2018-10-17 12:58:08 -0700

    Updated Server.go.

--- MuxServer/Server.go
+++ MuxServer/Server.go
@@ -89,7 +89,7 @@ func main() {
 	mux := mux.NewRouter()
 	rokulib.InitLib()
 	mux.HandleFunc("/roku", RokuServer)
-	loggedRouter := handlers.CombinedLoggingHandler(rokulib.Log, mux)
+	loggedRouter := handlers.CombinedLoggingHandler(rokulib.RokulibLog, mux)
 	//mux.Use(handlers.CombinedLoggingHandler(os.StdOut, ))
 	caCert, err := ioutil.ReadFile("/etc/httpsServer/ssl/certs/CAcerts.pem")
 	caCertPool := x509.NewCertPool()
