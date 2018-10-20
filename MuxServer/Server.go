6c0221d6b429a40bb7dc09f0810955401cc3c768
Veeru Natarajan <veeruns@yahoo.com>
2018-10-20 13:19:44 -0700

    Updated Server.go.

--- MuxServer/Server.go
+++ MuxServer/Server.go
@@ -157,7 +157,7 @@ func main() {
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 		},
-		MinVersion:         tls.VersionTLS12,
+		MinVersion:         tls.VersionTLS13,
 		ClientAuth:         tls.RequireAndVerifyClientCert,
 		InsecureSkipVerify: true,
 		ClientCAs:          caCertPool,
