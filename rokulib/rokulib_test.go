62d65c965c0127e7497078decc2f9d9c3489c376
Veeru Natarajan <veeruns@yahoo.com>
2018-10-22 11:08:49 -0700

    Updated rokulib_test.go.

--- rokulib/rokulib_test.go
+++ rokulib/rokulib_test.go
@@ -18,7 +18,7 @@ func TestReadConfig(t *testing.T) {
 
 func TestWrongDir(t *testing.T) {
 	var c Config
-	op := readconfig(&c, "/opt/httpsServer/confs", "Server")
+	op := readconfig(&c, "/opt/httpsServer/confs", "Servers")
 	fmt.Printf("Output is %v\n", op)
 	if c.Certdir != "/opt/httpsServer/conf/certs" {
 		t.Error("Expected /opt/httpsServer/conf/certs, there should not be anything here")
