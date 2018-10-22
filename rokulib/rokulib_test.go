e0fc130669333896221eab1e71b1139cf5ac0bcf
Veeru Natarajan <veeruns@yahoo.com>
2018-10-22 10:56:14 -0700

    Updated rokulib_test.go.

--- rokulib/rokulib_test.go
+++ rokulib/rokulib_test.go
@@ -14,3 +14,14 @@ func Testreadconfig(t *testing.T) {
 		t.Fatal("It should always be true")
 	}
 }
+
+func Testwrongdir(t *testing.T) {
+  var c Config
+	op := readconfig(&c, "/opt/httpsServer/conf", "Server")
+	if c.Certdir != "/opt/httpsServer/conf/cert" {
+		t.Error("Exepcted /opt/httpsServer/conf/certs")
+	}
+	if op == false {
+		t.Fatal("There should not be anything there)
+	}
+}
