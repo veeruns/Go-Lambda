69ed09c44d252225d02c4c5b300a2fbdf7e1d05c
Veeru Natarajan <veeruns@yahoo.com>
2018-10-22 10:48:29 -0700

    Updated rokulib_test.go.

--- rokulib/rokulib_test.go
+++ rokulib/rokulib_test.go
@@ -7,7 +7,7 @@ import (
 func Testreadconfig(t *testing.T) {
 	var c Config
 	readconfig(&c)
-	if c.certdir != "/opt/httpsServer/conf/certs" {
+	if c.Certdir != "/opt/httpsServer/conf/certs" {
 		t.Error("Exepcted /opt/httpsServer/conf/certs")
 	}
 }
