8535cf621c7de8b72d47f2dce8b2e7a73296ea94
Veeru Natarajan <veeruns@yahoo.com>
2018-10-22 09:42:25 -0700

    Updated Server.go.

--- MuxServer/Server.go
+++ MuxServer/Server.go
@@ -198,7 +198,6 @@ func main() {
 		//		Handler:   mux,
 		TLSConfig: cfg,
 		Handler:   loggedRouter,
-		ErrorLog:  *log.Logger,
 	}
 	go http.ListenAndServe(httpv.String(), certManager.HTTPHandler(nil))
 
