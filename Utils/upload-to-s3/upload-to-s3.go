04ab3c9b78d9ebb2ec6fefda49691b11403cb8d0
Veeru Natarajan <veeruns@yahoo.com>
2018-11-01 15:46:51 -0700

    Updated upload-to-s3.go.

--- Utils/upload-to-s3/upload-to-s3.go
+++ Utils/upload-to-s3/upload-to-s3.go
@@ -1,66 +1,66 @@
-ipackage main
+package main
 
 import (
-    "bytes"
-    "log"
-    "net/http"
-    "os"
+	"bytes"
+	"log"
+	"net/http"
+	"os"
 
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 // TODO fill these in!
 const (
-    S3_REGION = "us-east-1"
-    S3_BUCKET = "image.dump"
+	S3_REGION = "us-east-1"
+	S3_BUCKET = "image.dump"
 )
 
 func main() {
 
-    // Create a single AWS session (we can re use this if we're uploading many files)
-    s, err := session.NewSession(&aws.Config{Region: aws.String(S3_REGION)})
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Create a single AWS session (we can re use this if we're uploading many files)
+	s, err := session.NewSession(&aws.Config{Region: aws.String(S3_REGION)})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Upload
-    filename:=os.Arg[1]
-    err = AddFileToS3(s, filename)
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Upload
+	filename := os.Args[1]
+	err = AddFileToS3(s, filename)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // AddFileToS3 will upload a single file to S3, it will require a pre-built aws session
 // and will set file info like content type and encryption on the uploaded file.
 func AddFileToS3(s *session.Session, fileDir string) error {
 
-    // Open the file for use
-    file, err := os.Open(fileDir)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
+	// Open the file for use
+	file, err := os.Open(fileDir)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
-    // Get file size and read the file content into a buffer
-    fileInfo, _ := file.Stat()
-    var size int64 = fileInfo.Size()
-    buffer := make([]byte, size)
-    file.Read(buffer)
+	// Get file size and read the file content into a buffer
+	fileInfo, _ := file.Stat()
+	var size int64 = fileInfo.Size()
+	buffer := make([]byte, size)
+	file.Read(buffer)
 
-    // Config settings: this is where you choose the bucket, filename, content-type etc.
-    // of the file you're uploading.
-    _, err = s3.New(s).PutObject(&s3.PutObjectInput{
-        Bucket:               aws.String(S3_BUCKET),
-        Key:                  aws.String(fileDir),
-        ACL:                  aws.String("private"),
-        Body:                 bytes.NewReader(buffer),
-        ContentLength:        aws.Int64(size),
-        ContentType:          aws.String(http.DetectContentType(buffer)),
-        ContentDisposition:   aws.String("attachment"),
-        ServerSideEncryption: aws.String("AES256"),
-    })
-    return err
+	// Config settings: this is where you choose the bucket, filename, content-type etc.
+	// of the file you're uploading.
+	_, err = s3.New(s).PutObject(&s3.PutObjectInput{
+		Bucket:               aws.String(S3_BUCKET),
+		Key:                  aws.String(fileDir),
+		ACL:                  aws.String("private"),
+		Body:                 bytes.NewReader(buffer),
+		ContentLength:        aws.Int64(size),
+		ContentType:          aws.String(http.DetectContentType(buffer)),
+		ContentDisposition:   aws.String("attachment"),
+		ServerSideEncryption: aws.String("AES256"),
+	})
+	return err
 }
