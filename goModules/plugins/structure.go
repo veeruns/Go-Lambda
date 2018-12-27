e96d10fac16cde4370ed0452b486a8c6db0d4803
Veera Natarajan <veeruns@yahoo.com>
2018-12-27 10:35:51 -0800

    Updated structure.go.

---
+++ goModules/plugins/structure.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "fmt"
+)
+
+type result struct {
+  outputDesc string
+  outputCode int
+  outputString string
+}
+
+func (re []result) PrintResult{
+
+}
+
+var Result result
