fc34b1916f559fc8f55ea4be9cb97a60f4f24185
Veera Natarajan <veeruns@yahoo.com>
2018-12-27 12:01:02 -0800

    Updated groucho.go.

---
+++ goModules/Groucho/groucho.go
@@ -0,0 +1,35 @@
+package groucho
+
+import "fmt"
+
+type code int
+
+const (
+	CRITICAL code = iota
+	WARNING
+	OK
+)
+
+type result struct {
+	outputDesc   string
+	outputCode   int
+	outputString string
+}
+
+func (re result) PrintResult() {
+	fmt.Printf("OutputCode is %d\n", re.outputCode)
+	fmt.Printf("OutputString is %s\n", re.outputString)
+	fmt.Printf("OutputDesc is %s\n", re.outputDesc)
+
+}
+
+func (re result) Validate() (bool, string) {
+	if len(re.outputDesc) == 0 || len(re.outputString) == 0 || re.outputCode > 3 || re.outputCode < 0 {
+		return false, "Something Wrong with result set"
+	}
+	return true, "All OK"
+
+}
+
+// Result is the main stucture
+var Result result
