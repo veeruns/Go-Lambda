074eb1f6f825eb5cdeef90b6fcbfee013c101bc6
Veera Natarajan <veeruns@yahoo.com>
2018-12-27 12:17:32 -0800

    Updated groucho.go.

--- goModules/Groucho/groucho.go
+++ goModules/Groucho/groucho.go
@@ -10,26 +10,23 @@ const (
 	OK
 )
 
-type result struct {
+type Result struct {
 	outputDesc   string
 	outputCode   int
 	outputString string
 }
 
-func (re result) PrintResult() {
+func (re Result) PrintResult() {
 	fmt.Printf("OutputCode is %d\n", re.outputCode)
 	fmt.Printf("OutputString is %s\n", re.outputString)
 	fmt.Printf("OutputDesc is %s\n", re.outputDesc)
 
 }
 
-func (re result) Validate() (bool, string) {
+func (re Result) Validate() (bool, string) {
 	if len(re.outputDesc) == 0 || len(re.outputString) == 0 || re.outputCode > 3 || re.outputCode < 0 {
 		return false, "Something Wrong with result set"
 	}
 	return true, "All OK"
 
 }
-
-// Result is the main stucture
-var Result result
