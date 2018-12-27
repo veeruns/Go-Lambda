04679d60bb55380dec217adab4b6cc593494f091
Veera Natarajan <veeruns@yahoo.com>
2018-12-27 14:20:32 -0800

    Updated groucho.go.

--- goModules/Groucho/groucho.go
+++ goModules/Groucho/groucho.go
@@ -61,8 +61,8 @@ func (re Result) PrintAllResults() error {
 
 		return errors.New(op)
 	}
-  for k := range AllResults {
-      fmt.Printf("%s %s %d\n",k[])
-  }
+	for _, v := range AllResults {
+		fmt.Printf("%s %s %d\n", v.OutputString, v.OutputDesc, v.OutputCode)
+	}
 	return nil
 }
