f2ee95d1833aa61c87dc8984472359c8a636560b
Veera Natarajan <veeruns@yahoo.com>
2018-12-27 10:42:53 -0800

    Updated structure.go.

--- goModules/plugins/structure.go
+++ goModules/plugins/structure.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type code int
+const (
+  CRITICAL code = iota
+  WARNING
+  OK
+)
 type result struct {
 	outputDesc   string
 	outputCode   int
@@ -13,6 +19,10 @@ func (re result) PrintResult() {
 	fmt.Printf("OutputString is %s\n", re.outputString)
 	fmt.Printf("OutputDesc is %s\n", re.outputDesc)
 
+}
+
+func (re result) Validate(){
+  if(len(re.outputDesc) == 0 || len(re.outputString) == 0) ||
 }
 
 var Result result
