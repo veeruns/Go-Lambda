9f7519ba89aa833ec80db1636d882b99a1c0fd11
Veera Natarajan <veeruns@yahoo.com>
2018-12-28 14:30:34 -0800

    Updated loop.go.

--- goModules/loop.go
+++ goModules/loop.go
@@ -1,1 +1,15 @@
 package main
+
+type plugin struct {
+	pluginname string
+	pluginpath string
+	pluginlog  string
+}
+type Config struct {
+	logfile string
+	plugins []plugin
+}
+
+func main() {
+
+}
