beebcb74d03e54346b36a08819056725763bf5f9
Veera Natarajan <veeruns@yahoo.com>
2018-12-28 09:29:01 -0800

    Updated disk.go.

--- goModules/plugins/disk.go
+++ goModules/plugins/disk.go
@@ -12,6 +12,17 @@ func main() {
 	disk.RegisterID.CodePath = "github.com/veeruns/Go-Lambda/goModules/plugins"
 	disk.OutputCode = groucho.OK
 	disk.OutputDesc = "Test result"
+
+	var d2 groucho.Result
+	d2.OutputString = "test2"
+	d2.OutputDesc = "Test2 op"
+	d2.OutputCode = groucho.WARNING
+	d2.RegisterID.Name = "DiskChecker"
+	d2.RegisterID.Version = "2.0"
+
+	var allr groucho.AllResults
+	allr.Append
+
 	op, _ := disk.Validate()
 	if op == true {
 		disk.PrintResult()
