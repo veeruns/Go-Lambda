af826b37de43bae1387d2d263626f1131e572366
Veera Natarajan <veeruns@yahoo.com>
2019-05-03 11:44:01 -0700

    Updated servicenow.go.

--- servicenow.go
+++ servicenow.go
@@ -14,6 +14,8 @@ func basicAuth() string {
 	req, err := http.NewRequest("GET", "https://vzbuilders.service-now.com/api/now/table/u_ops_request_management", nil)
 	req.SetBasicAuth(username, passwd)
 	query := req.URL.Query()
+	query.Add("sysparm_query", "u_business_service%3D8434f15b0fc0cfc0abe3590be1050e86^state=1")
+	query.Add("sysparam_limit", 10)
 
 	resp, err := client.Do(req)
 	if err != nil {
